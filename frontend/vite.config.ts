46142fb712b3d0daea8c26d8530e9026b06484a7
Nikoloz <nikavarme32@gmail.com>
2026-02-10 17:15:24 +0400

    serveristvis

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // ძალიან მნიშვნელოვანია Netlify-ზე correct paths-ისთვის
+});
